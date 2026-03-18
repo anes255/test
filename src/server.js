ea83d5061e4c9221af1bd3029e36a806f3442b90
anes255 <anesgaher3000@gmail.com>
2026-03-18 02:11:44 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 app.use(compression());
 app.use(cookieParser());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // ============ ROOT & HEALTH ============
