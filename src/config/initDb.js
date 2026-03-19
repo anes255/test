44d86d6576c4827b5eec97f68fc04e298d2b41ca
anes255 <anesgaher3000@gmail.com>
2026-03-19 01:50:55 +0100

    Delete src directory

--- src/config/initDb.js
+++
@@ -1,3 +0,0 @@
-const pool=require('./db');
-const initDb=async()=>{const r=await pool.query("SELECT table_name FROM information_schema.tables WHERE table_schema='public'");console.log('DB:',r.rows.length,'tables');};
-module.exports={initDb};
