3a74a063178906ef6d44348557e338ef60049e1e
anes255 <anesgaher3000@gmail.com>
2026-03-19 01:23:27 +0100

    Delete src directory

--- src/config/db.js
+++
@@ -1,4 +0,0 @@
-const{Pool}=require('pg');require('dotenv').config();
-const pool=new Pool({connectionString:process.env.DATABASE_URL,ssl:{rejectUnauthorized:false},max:10});
-pool.on('error',e=>console.error('DB:',e.message));
-module.exports=pool;
